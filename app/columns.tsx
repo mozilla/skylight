3387335d8b133965eddac97d938cac3987b0e543
Dan Mosedale <dmose2@gmail.com>
2023-12-31 20:23:31 -0800

    Stand up DataTable-version of MessageTable

--- app/columns.tsx
+++ app/columns.tsx
@@ -4,26 +4,44 @@ import { ColumnDef } from "@tanstack/react-table"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Experiment = {
-  slug: string
+export type Message = {
+  product: 'Desktop' | 'Android'
+  release: string
   displayName: string
+  id: string
   topic: string
   surface: string
   segment: string
-  metrics: string
+  ctrPercent: string
+  ctrPercentChange: string
+  ctrDashboard: string
+  previewLink: string
 }
 
-export const columns: ColumnDef<Experiment>[] = [
+export const columns: ColumnDef<Message>[] = [
   {
-    accessorKey: "displayName",
-    header: "Name",
+    accessorKey: "release",
+    header: "Release",
   },
   {
-    accessorKey: "metrics",
+    accessorKey: "id",
+    header: "Message ID",
+  },
+  {
+    accessorKey: "topic",
+    header: "Topic",
+  },
+  {
+    accessorKey: "surface",
+    header: "Surface",
+  },{
+    accessorKey: "segment",
+    header: "Segment",
+  },{
+    accessorKey: "Metrics",
     header: "Metrics",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
+  },{
+    accessorKey: "references",
+    header: "References",
   },
 ]
