f072932ab5fa3551636c17c9e7a22ea4763ddd8f
Dan Mosedale <dmose2@gmail.com>
2024-02-01 10:08:04 -0800

    Configure jest from next.js docs

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,12 @@
  */
 
 import type {Config} from 'jest';
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -196,4 +202,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
