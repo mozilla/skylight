1ae2733a03fdf40b57e5dcea0f924d2317c650fc
Dan Mosedale <dmose2@gmail.com>
2024-02-19 19:35:20 -0800

    Bug 1880334 - Add infobar dashboards

--- lib/messageUtils.ts
+++ lib/messageUtils.ts
@@ -28,10 +28,25 @@ export function getTemplateFromMessage(msg : any) : string {
   return msg.template;
 }
 
-export function isAboutWelcomeTemplate( template : string ) : boolean {
+export function _isAboutWelcomeTemplate( template : string ) : boolean {
   // XXX multi shouldn't really be here, but for now, we're going to assume
   // it's a spotlight
   const aboutWelcomeSurfaces = ['feature_callout', 'multi', 'spotlight']
 
   return aboutWelcomeSurfaces.includes(template);
+}
+
+export function getDashboard( template: string, msgId: string ) : string | undefined {
+  const encodedMsgId = encodeURIComponent(msgId);
+  const encodedTemplate = encodeURIComponent(template);
+
+  if (_isAboutWelcomeTemplate(template)) {
+    return `https://mozilla.cloud.looker.com/dashboards/1471?Message+ID=%25${encodedMsgId?.toUpperCase()}%25`
+  }
+
+  if (template === 'infobar') {
+    return `https://mozilla.cloud.looker.com/dashboards/1622?Messaging+System+Ping+Type=${encodedTemplate}&Submission+Date=30+days&Messaging+System+Message+Id=${encodedMsgId}&Normalized+Channel=release&Normalized+OS=&Client+Info+App+Display+Version=&Normalized+Country+Code=`
+  }
+
+  return undefined
 }
