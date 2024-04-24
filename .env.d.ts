dda8593234451ec28dd89a0a2d8ef5af18292ab2
Emily McMinn <emcminn@mozilla.com>
2024-04-24 17:22:58 -0400

    fix the env declaration

--- .env.d.ts
+++ .env.d.ts
@@ -2,8 +2,7 @@
 
 namespace NodeJS {
   interface ProcessEnv {
-    PROD_RECORDS_URL: string;
-    PREVIEW_RECORDS_URL: string;
+    RECORDS_URL: string;
     NODE_ENV: 'development' | 'production';
   }
 }
