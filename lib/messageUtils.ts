15947d8f3f8b318548419c3f9f306e15ed7a7fc5
Sarah Chung <sachung@mozilla.com>
2024-05-06 12:23:29 -0400

    Change (screen 1) to (1st screen)

--- lib/messageUtils.ts
+++ lib/messageUtils.ts
@@ -1,7 +1,7 @@
 export function getDisplayNameForTemplate(template: string): string {
   const displayNames: any = {
     aboutwelcome: "About:Welcome Page",
-    feature_callout: "Feature Callout (screen 1)",
+    feature_callout: "Feature Callout (1st screen)",
     infobar: "InfoBar",
     milestone_message: "Milestone Messages",
     multi: "1st of Multiple Messages",
