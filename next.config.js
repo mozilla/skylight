ef1e3647fcd82fc8022762365038e5d6caf1bd52
Sarah Chung <sachung@mozilla.com>
2024-05-22 12:44:31 -0400

    Add next config headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: 'frame-ancestors="none"',
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+            ],
+          },
+        ]
+      },
+}
 
 module.exports = nextConfig
