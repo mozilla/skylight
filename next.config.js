f03ccce2f8a69f0ef197daeab181d1178a78f71f
Sarah Chung <sachung@mozilla.com>
2024-05-22 15:03:19 -0400

    Add content security policy header

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
           {
-            source: '/:path*',
-            headers: [
-              {
-                key: 'Content-Security-Policy',
-                value: 'frame-ancestors="none"',
-              },
-              {
-                key: 'X-Frame-Options',
-                value: 'DENY',
-              },
-            ],
+            key: "Content-Security-Policy",
+            value:
+              "Content-Security-Policy: default-src https: 'self' *.netlify.app; font-src 'self' fonts.gstatic.com; img-src 'self' data: blob:; script-src https: 'self' 'unsafe-inline' 'unsafe-eval'; style-src https://cdn.tailwindcss.com/ https: 'self' 'unsafe-inline'; frame-ancestors 'self'",
           },
-        ]
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
       },
-}
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
