83c0427ab80af93dc0367c0a305422195786d152
Sarah Chung <sachung@mozilla.com>
2024-05-22 15:09:26 -0400

    Fix csp header

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "Content-Security-Policy: default-src https: 'self' *.netlify.app; font-src 'self' fonts.gstatic.com; img-src 'self' data: blob:; script-src https: 'self' 'unsafe-inline' 'unsafe-eval'; style-src https://cdn.tailwindcss.com/ https: 'self' 'unsafe-inline'; frame-ancestors 'self'",
+              "default-src https: 'self' *.netlify.app; font-src 'self' fonts.gstatic.com; img-src 'self' data: blob:; script-src https: 'self' 'unsafe-inline' 'unsafe-eval'; style-src https://cdn.tailwindcss.com/ https: 'self' 'unsafe-inline'; frame-ancestors 'self'",
           },
           {
             key: "X-Frame-Options",
