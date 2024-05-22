9e01cd19c5ac44d99aa44c1ef9c8aedac0607275
Sarah Chung <sachung@mozilla.com>
2024-05-22 15:29:47 -0400

    Add to csp header

--- next.config.js
+++ next.config.js
@@ -4,9 +4,12 @@ const cspHeader = `
     default-src https: 'self' *.netlify.app;
     font-src 'self';
     img-src 'self' blob: data:;
-    script-src https: 'self' 'unsafe-inline' 'unsafe-eval';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval';
     style-src https://cdn.tailwindcss.com/ https: 'self' 'unsafe-inline';
     frame-ancestors 'self';
+    base-uri 'self';
+    form-action 'self';
+    object-src 'none';
 `
 
 const nextConfig = {
