f47f58cddc4ee3771b6de35d01aaf416f3ade0b6
Sarah Chung <sachung@mozilla.com>
2024-05-23 12:55:13 -0400

    Separate dev and prod headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const cspHeader = `
+const isDev = process.env.NODE_ENV !== "production";
+
+const cspHeaderProd = `
+    default-src 'none';
+    img-src 'self' blob: data:;
+    connect-src 'self';
+    font-src 'self';
+    script-src 'self' 'unsafe-inline';
+    style-src 'self' https://cdn.tailwindcss.com/;
+    frame-ancestors 'self';
+    base-uri 'self';
+    form-action 'self';
+    object-src 'none';
+`;
+
+const cspHeaderDev = `
     default-src 'none';
     img-src 'self' blob: data:;
     connect-src 'self';
@@ -11,7 +26,7 @@ const cspHeader = `
     base-uri 'self';
     form-action 'self';
     object-src 'none';
-`
+`;
 
 const nextConfig = {
   async headers() {
@@ -21,7 +36,9 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ''),
+            value: isDev
+              ? cspHeaderDev.replace(/\n/g, "")
+              : cspHeaderProd.replace(/\n/g, ""),
           },
           {
             key: "X-Frame-Options",
