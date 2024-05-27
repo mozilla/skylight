af1c20d60170e99073b2ba4053e76e39b4439f39
Sarah Chung <sachung@mozilla.com>
2024-05-27 12:44:44 -0400

    Add unit tests

--- components/ui/infopopover.tsx
+++ components/ui/infopopover.tsx
@@ -8,14 +8,14 @@ import { Info } from "lucide-react";
 type InfoProps = {
     iconSize: number;
     content: any;
-    iconStyle: string;
+    iconStyle?: string;
   };
   
   export function InfoPopover({ iconSize, content, iconStyle }: InfoProps) {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Info size={iconSize} className={iconStyle}/>
+        <Info size={iconSize} className={iconStyle} role="info"/>
       </PopoverTrigger>
       <PopoverContent className="w-80">
         <div className="grid gap-4">
