0bc97bbb82991f626702e1325ea5716820437147
Sarah Chung <sachung@mozilla.com>
2024-06-20 16:30:28 -0400

    Linting

--- lib/looker.ts
+++ lib/looker.ts
@@ -26,17 +26,17 @@ export async function runEventCountQuery(filters: any, template: string): Promis
   if (template === "infobar") {
     newQueryBody.filters = Object.assign(
       {
-        'messaging_system.submission_date': '1 day ago for 1 day'
+        "messaging_system.submission_date": "1 day ago for 1 day",
       },
       filters
-    )
+    );
   } else {
     newQueryBody.filters = Object.assign(
       {
-        'event_counts.submission_timestamp_date': '2 days'
+        "event_counts.submission_timestamp_date": "2 days",
       },
       filters
-    )
+    );
   }
 
   const newQuery = await SDK.ok(SDK.create_query(newQueryBody));
@@ -58,26 +58,37 @@ export async function runEventCountQuery(filters: any, template: string): Promis
  * @returns a CTR percent value for a message if the Looker query results are
  * defined
  */
-export async function getCTRPercent(id: string, template: string, channel?: string, experiment?: string, branch?: string): Promise<number|undefined> {
+export async function getCTRPercent(
+  id: string,
+  template: string,
+  channel?: string,
+  experiment?: string,
+  branch?: string
+): Promise<number | undefined> {
   let queryResult;
   if (template === "infobar") {
     queryResult = await runEventCountQuery(
-      { 'messaging_system.metrics__text2__messaging_system_message_id': id,
-        'messaging_system.normalized_channel': channel,
-        'messaging_system.metrics__string__messaging_system_ping_type': template },
+      {
+        "messaging_system.metrics__text2__messaging_system_message_id": id,
+        "messaging_system.normalized_channel": channel,
+        "messaging_system.metrics__string__messaging_system_ping_type":
+          template,
+      },
       template
-    )
+    );
   } else {
     queryResult = await runEventCountQuery(
-      { 'event_counts.message_id':  '%' + id + '%',
-        'event_counts.normalized_channel': channel,
-        'onboarding_v1__experiments.experiment': experiment,
-        'onboarding_v1__experiments.branch': branch },
+      {
+        "event_counts.message_id": "%" + id + "%",
+        "event_counts.normalized_channel": channel,
+        "onboarding_v1__experiments.experiment": experiment,
+        "onboarding_v1__experiments.branch": branch,
+      },
       template
-    )
+    );
   }
 
   if (queryResult.length > 0) {
-    return Number(Number(queryResult[0].primary_rate * 100).toFixed(1))
+    return Number(Number(queryResult[0].primary_rate * 100).toFixed(1));
   }
 }
