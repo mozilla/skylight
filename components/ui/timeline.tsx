b5ad79d4c9b34b17ee9f7c67221b10df6243cae8
Sarah Chung <sachung@mozilla.com>
2024-07-15 10:07:05 -0400

    Address pr comments

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -1,42 +1,32 @@
+type TimelineStepProps = {
+  isActive: boolean;
+  stepName: string;
+};
+
 type TimelineProps = {
   active: string;
 };
 
+function TimelineStep({ isActive, stepName }: TimelineStepProps) {
+  const activeColorStyle = isActive
+    ? "border-blue-300 text-blue-500"
+    : "border-grey text-slate-500";
+
+  return (
+    <div className={`text-sm border-2 px-2 rounded-full ` + activeColorStyle}>
+      {stepName}
+    </div>
+  );
+}
+
 export function Timeline({ active }: TimelineProps) {
   return (
     <div className="flex flex-row items-center justify-center mt-2">
-      <p
-        className={
-          `text-sm border-2 px-2 rounded-full ` +
-          (active === "experiment"
-            ? "border-blue-300 text-blue-500"
-            : "border-grey text-slate-500")
-        }
-      >
-        Experiment
-      </p>
+      <TimelineStep isActive={active === "experiment"} stepName="Experiment" />
       <hr className="border-1 w-4" />
-      <p
-        className={
-          `text-sm border-2 px-2 rounded-full ` +
-          (active === "rollout"
-            ? "border-blue-300 text-blue-500"
-            : "border-grey text-slate-500")
-        }
-      >
-        Rollout
-      </p>
+      <TimelineStep isActive={active === "rollout"} stepName="Rollout" />
       <hr className="border-1 w-4" />
-      <p
-        className={
-          `text-sm border-2 px-2 rounded-full ` +
-          (active === "in tree"
-            ? "border-blue-300 text-blue-500"
-            : "border-grey text-slate-500")
-        }
-      >
-        In Tree
-      </p>
+      <TimelineStep isActive={active === "in tree"} stepName="In Tree" />
     </div>
   );
 }
