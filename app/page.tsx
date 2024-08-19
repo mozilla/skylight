5900294557c27cee4fa05617f0d675e8e1ef6d86
Sarah Chung <sachung@mozilla.com>
2024-08-19 12:57:03 -0400

    Add comments

--- app/page.tsx
+++ app/page.tsx
@@ -114,6 +114,10 @@ let columnsShown = false;
 
 type NimbusExperiment = types.experiments.NimbusExperiment;
 
+/**
+ * Updates the lib/looker-local-prod-messages/129-nightly.json file with the
+ * JSON data retrieved from the Looker query for live messages.
+ */
 async function updateLookerLiveMessagesList() {
   let data = await runLookQuery();
   const fs = require("fs");
@@ -125,6 +129,9 @@ async function updateLookerLiveMessagesList() {
   );
 }
 
+/**
+ * Returns the list of live messages to display in the live message table.
+ */
 async function getLookerLiveMessages() {
   let data = await runLookQuery();
 
