37b9767ecd3238398457c16ff9c38587e14b6b16
Dan Mosedale <dmose2@gmail.com>
2025-03-18 09:04:11 -0700

    Appease prettier more

--- __tests__/app/dashboard.test.tsx
+++ __tests__/app/dashboard.test.tsx
@@ -13,7 +13,7 @@ global.fetch = jest.fn(() =>
 
 describe("Dashboard", () => {
   it("all timeline pill ids exist in the Dashboard component in /", async () => {
-    const dashboard = render(await <Dashboard platform="desktop" />);
+    const dashboard = render(await (<Dashboard platform="desktop" />));
 
     const firefox = dashboard.getByTestId("firefox");
     const experiments = dashboard.getByTestId("live_experiments");
