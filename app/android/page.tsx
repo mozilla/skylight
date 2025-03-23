f7e8cf5eb28919a1ee1b2f84f250d68657c7d396
Dan Mosedale <dmose2@gmail.com>
2025-03-22 19:48:37 -0700

    Fixup Android calling of Dashboard component.

--- app/android/page.tsx
+++ app/android/page.tsx
@@ -1,6 +1,22 @@
 import { fetchData, Dashboard } from "@/app/dashboard";
 
 export default async function Page() {
-  const data = await fetchData();
-  return <Dashboard platform={"android"} {...data} />;
+  const {
+    localData,
+    experimentAndBranchInfo,
+    totalExperiments,
+    msgRolloutInfo,
+    totalRolloutExperiments,
+  } = await fetchData();
+
+  return (
+    <Dashboard
+      platform="android"
+      localData={localData}
+      experimentAndBranchInfo={experimentAndBranchInfo}
+      totalExperiments={totalExperiments}
+      msgRolloutInfo={msgRolloutInfo}
+      totalRolloutExperiments={totalRolloutExperiments}
+    />
+  );
 }
