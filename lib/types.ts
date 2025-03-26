601d3cd6ac18ec00f8e1323459052613480f9bb7
Dan Mosedale <dmose2@gmail.com>
2025-03-26 13:36:07 -0700

    Standarize platform typing to the nimbus platform slug strings

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,5 @@
-// These are used as strings in the UI, so we want to force them to be
-// consistent and not have to worry about typos.
-export type Platform = "Android" | "iOS" | "Desktop";
+// These are the same strings that the experimenter API uses to determine which
+// endpoint to hit. XXX we should use our own ID and put this in
+// PlatformInfo in case Nimbus changes its strings.
+
+export type Platform = "fenix" | "ios" | "firefox-desktop";
