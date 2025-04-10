fd1e8cb318c65aefdb7b5fc25999a28dce865a32
Sarah Chung <sachung@mozilla.com>
2025-04-10 10:55:57 -0400

    Remove unused imports in test

--- __tests__/app/message-table.test.tsx
+++ __tests__/app/message-table.test.tsx
@@ -1,10 +1,4 @@
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import { MessageTable } from "@/app/message-table";
 import {
   completedExperimentColumns,
