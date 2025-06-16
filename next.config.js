de64726c917e42780731d8c9f148c53c44e69bad
Dan Mosedale <dmose2@gmail.com>
2025-06-16 10:37:38 -0700

    Up staticPageGenerationTimeout again to avoid limit

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const cspHeaderDev = `
 `;
 
 const nextConfig = {
-  staticPageGenerationTimeout: 480,
+  staticPageGenerationTimeout: 960,
   eslint: {
     dirs: ["pages", "app", "components", "lib", "src", "__tests__"],
   },
